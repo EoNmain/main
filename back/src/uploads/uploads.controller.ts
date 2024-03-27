5dc0ce1a3a99a96743a67bedb58a4fa55043f621
shs9267 <shs9267@gmail.com>
2024-03-27 11:29:13 +0900

    controllertoService
    controller와 service 나눔

--- back/src/uploads/uploads.controller.ts
+++ back/src/uploads/uploads.controller.ts
@@ -1,37 +1,14 @@
-import {
-    Controller,
-    Post,
-    UploadedFile,
-    UseInterceptors,
-  } from '@nestjs/common';
-  import { FileInterceptor } from '@nestjs/platform-express';
-  import * as AWS from 'aws-sdk';
-  
-  const BUCKET_NAME = 'eontest-bucket';
-  
-  @Controller('uploads')
-  export class UploadsController {
-    @Post('')
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadFile(@UploadedFile() file) {
-      AWS.config.update({
-        region: 'ap-northeast-2',
-        credentials: {
-          accessKeyId: process.env.AWS_IAM_ACCESS_KEY,
-          secretAccessKey: process.env.AWS_IAM_SECRET_KEY,
-        },
-      });
-      try {
-        const upload = await new AWS.S3()
-          .putObject({
-            Key: `${Date.now() + file.originalname}`,
-            Body: file.buffer,
-            Bucket: BUCKET_NAME,
-          })
-          .promise();
-        console.log(upload);
-      } catch (error) {
-        console.log(error);
-      }
-    }
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadsService } from './uploads.service';
+
+@Controller('uploads')
+export class UploadsController {
+  constructor(private readonly uploadService: UploadsService) {}
+
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    return await this.uploadService.uploadFile(file);
   }
+}
--- back/src/uploads/uploads.service.ts
+++ back/src/uploads/uploads.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
-export class UploadsService {}
+export class UploadsService {
+  private readonly BUCKET_NAME = 'eontest-bucket';
+
+  constructor() {
+    AWS.config.update({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: process.env.AWS_IAM_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_IAM_SECRET_KEY,
+      },
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    try {
+      const upload = await new AWS.S3()
+        .putObject({
+          Key: `${Date.now() + file.originalname}`,
+          Body: file.buffer,
+          Bucket: this.BUCKET_NAME,
+        })
+        .promise();
+      //console.log(upload);
+      return upload; // Upload 정보를 반환하거나 필요에 따라 다른 작업을 수행할 수 있습니다.
+    } catch (error) {
+      console.log(error);
+      throw error; // 에러를 처리하거나 호출하는 쪽으로 에러를 전파할 수 있습니다.
+    }
+  }
+}
